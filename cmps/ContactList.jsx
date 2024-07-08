c56a18f14333ed551af9e82d3034377a9dade9b5
Dolev Levy <Dolevlevy97@gmail.com>
2024-07-08 15:56:15 +0300

    Implement list and preview

--- cmps/ContactList.jsx
+++ cmps/ContactList.jsx
@@ -1,3 +1,5 @@
+import { ContactPreview } from "./ContactPreview.jsx"
+
 const { Link } = ReactRouterDOM
 
 
@@ -5,8 +7,7 @@ export function ContactList({ contacts, onDeleteContact }) {
     return (
         contacts.map(contact =>
             <article className="contact-container" key={contact._id}>
-                <h1>{contact.fullName}</h1>
-                <h2>{contact.number}</h2>
+                <ContactPreview contact={contact} />
                 <section className="btns">
                     <button className='btn btn-details'><Link to={`/contact/${contact._id}`}>Details</Link></button>
                     <button className="btn-edit">Edit</button>
